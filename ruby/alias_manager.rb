6223d3d96480ec292403aab75c15b9afd7aa4eac
wushuchris <chrismendoza8@gmail.com>
2016-08-09 10:38:56 -0700

    stored encrypted names in hash

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -33,8 +33,17 @@ def next_letter(reversed_name)
   reversed_name_array_joined = reversed_name_array.join
 end
 
-#Release 1 - UI
-##Provide a user interface that lets a user enter a name and get a fake name back. Let the user do this repeatedly until they decide to quit by typing 'quit'. (They might just hit Enter to continue.)
+#name printer
+def print_names(names_hash)
+  puts "Here are your name and encrypted name pairs"
+  names_hash.each do |name, encrypted|
+    puts "#{name} : #{encrypted}"
+  end
+end
+
+
+#hash to store names of real names and encrypted names
+names_hash = {}
 
 loop do
   puts "Please Enter the Name you would like to encrypt, ('quit' to stop)"
@@ -42,7 +51,18 @@ loop do
   break if name == 'quit'
   reversed_name = swap_first_last(name)
   encrypted_name = next_letter(reversed_name)
-  puts "The encrypted name is #{encrypted_name}"
+  name_symbol = name.to_sym
+  names_hash[name_symbol] = encrypted_name
+  puts "#{name} has been encrypted"
 end
+
+print_names(names_hash)
+
+
+
+#Release 1 - UI
+##Provide a user interface that lets a user enter a name and get a fake name back. Let the user do this repeatedly until they decide to quit by typing 'quit'. (They might just hit Enter to continue.)
+
+#Release 2 - Use a data structure to store the fake names as they are entered. When the user exits the program, iterate through the data structure and print all of the data the user entered. A sentence like "Vussit Gimodoe is actually Felicia Torres" or "Felicia Torres is also known as Vussit Gimodoe" for each agent is fine.
 
 
