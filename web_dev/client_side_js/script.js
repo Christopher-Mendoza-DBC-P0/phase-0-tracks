2bfdc38ed9a6b9faec5d2b0e0482923073c474d7
wushuchris <chrismendoza8@gmail.com>
2016-09-08 12:39:19 -0700

    a bit of java script 9.2

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,1 +1,10 @@
 console.log("The script is running!");
+
+function addGreenBorder() {
+  var photo = document.getElementById("plane-success");
+  photo.style.border = "10px solid green";
+}
+
+var photo = document.getElementById("plane-success");
+photo.addEventListener("mouseover", addGreenBorder);
+
